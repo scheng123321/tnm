d8621fc0e418dab4e1b3765f69c4b143ec89770c
Simon Cheng <simoncheng559@gmail.com>
2018-11-11 17:51:42 +0000

    Changed port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = 8080 || process.env.PORT;
 app.use(express.static('public'));
-app.listen(process.env.PORT);
-console.log('App listening on '+process.env.PORT);
+app.listen(port);
+console.log('App listening on '+port);
